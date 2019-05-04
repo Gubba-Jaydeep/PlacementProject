508767a27378f3ee9e1d11bc531fee4d7f7b8cf4
Gubba-Jaydeep <jaydeepgubba@gmail.com>
2019-05-04 16:09:32 +0530

    added some models

--- Forum/mainapp/urls.py
+++ Forum/mainapp/urls.py
@@ -5,4 +5,10 @@ from . import views
 app_name="mainapp"
 urlpatterns=[
     path("",views.forum,name="forum"),
+    path("askQuestion", views.askQuestion, name="askQuestion"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("r_validate", views.r_validate, name="r_validate"),
+    path("logout", views.logout, name="logout"),
+
 ]
--- Forum/mainapp/views.py
+++ Forum/mainapp/views.py
@@ -10,8 +10,38 @@ def askQuestion(request):
     f = Forum()
     #newQuestion
     q={}
-    q['name']=request.GET['name']
+    q['uID']='16B81A05B8'
+    q['type']='student'
+    q['question']=request.GET['askedQuestion']
+    q['votes']=0
+    q['answers']=[]
+    q['subject']=request.GET['subject']
 
-    f.addQuestion(q)
-    questions = f.getQuestions()
-    return render(request, 'mainapp/forum.html',{"questions":questions})
+    if f.addQuestion(q):
+        questions = f.getQuestions()
+        return render(request, 'mainapp/forum.html',{"questions":questions})
+    return render(request, 'mainapp/forum1.html',{})
+
+def login(request):
+    f=Forum()
+    users=f.getUser()
+    return render(request,'mainapp/forum.html',{})
+
+def register(request):
+    return render(request, 'mainapp/forum.html', {})
+
+def r_validate(request):
+    return render(request,'mainapp/forum.html',{})
+
+
+
+def sendMail(email,otp):
+    '''sends mail provided email and otp'''
+    import smtplib
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login("apnnarayana@gmail.com", "aappnnaassss")
+    message = "welcome to cvr forum\n\nyout otp is:"+otp
+    s.sendmail("apnnarayana@gmail.com", email, message)
+    s.quit()
+
