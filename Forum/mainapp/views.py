2da81b1127e00ef6560bdfca155395b37c108d05
Gubba-Jaydeep <jaydeepgubba@gmail.com>
2019-05-04 17:36:58 +0530

    login logout

--- Forum/mainapp/views.py
+++ Forum/mainapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .models import Forum
 # Create your views here.
-def forum(request):
+def forum():
     f = Forum()
     questions=f.getQuestions()
-    return render(request, 'mainapp/forum.html',{"questions":questions})
+    return questions
 
 def askQuestion(request):
     f = Forum()
@@ -22,20 +22,44 @@ def askQuestion(request):
         return render(request, 'mainapp/forum.html',{"questions":questions})
     return render(request, 'mainapp/forum1.html',{})
 
+def index(request):
+    if request.COOKIES.get("loggedIn", None):
+        return render(request, 'mainapp/forum.html', {})
+
+    return render(request, 'mainapp/login.html', {})
+
 def login(request):
+    uID = request.GET['logUser']
+    psw =request.GET['logPass']
     f=Forum()
-    uID=12
-    users=f.getUser(uID)
-    return render(request,'mainapp/forum.html',{})
+    user = f.getUser(uID)
+    if user and user['password']==psw:
+        questions= forum()
+        response = render(request, 'mainapp/forum.html', {'questions': questions})
+        response.set_cookie("loggedIn", True)
+        response.set_cookie("userName", uID)
+        return response
+    else:
+        return render(request,'mainapp/forum1.html',{})
+def logout(request):
+    if request.COOKIES.get("loggedIn"):
+        response =  render(request, 'mainapp/login.html', {})
+        response.delete_cookie("userName")
+        response.delete_cookie("loggedIn")
+        return response
+    else:
+        render(request, 'mainapp/forum1.html', {})
 
 def register(request):
-    return render(request, 'mainapp/forum.html', {})
+    uId=request.GET['uID']
+    email=request.GET['email']
+    ctx={"uId":uId,"email":email}
+    return render(request, 'mainapp/forum.html', {"ctx":ctx})
 
 def r_validate(request):
     return render(request,'mainapp/forum.html',{})
 
-def logout(request):
-    return render(request, 'mainapp/forum.html', {})
+
 
 def sendMail(email,otp):
     '''sends mail provided email and otp'''
